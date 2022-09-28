0436329188309fa04fc074b273395ea9cae947bc
Ken Lenhart <omnipaid@gmail.com>
2022-09-28 00:53:20 -0700

    Add scope for lengthy SQL queries to invoice_item.rb

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -9,4 +9,8 @@ class InvoiceItem < ApplicationRecord
   has_many :bulk_discounts, through: :merchant
 
   enum status: { pending: 0, packaged: 1, shipped: 2}
+
+  scope :revenue, -> { sum('invoice_items.quantity * invoice_items.unit_price') }
+  scope :threshold, -> { where('invoice_items.quantity >= bulk_discounts.threshold') }
+  scope :discount, -> { select('invoice_items.id, max((invoice_items.quantity * invoice_items.unit_price) * bulk_discounts.discount) as discount') }
 end
