0382723fdd20f047a4f9aa294b0a11498636cd48
Ken Lenhart <omnipaid@gmail.com>
2022-09-27 20:43:17 -0700

    Add total_discount, total_revenue, total_discounted_revenue to invoice.rb with tests

--- spec/features/admin/bulk_discounts/invoices/show_spec.rb
+++ spec/features/admin/bulk_discounts/invoices/show_spec.rb
@@ -32,12 +32,49 @@ RSpec.describe 'admin invoice show page bulk discounts' do
       create(:invoice_items, invoice_id: invoice_2.id, item_id: item_6.id, unit_price: 15, quantity: 10) # 150
 
       visit admin_invoice_path(invoice_1)
+      
+      within "#total-revenue" do
+        expect(page).to have_content(invoice_1.total_revenue)
+      end
 
-      expect(invoice_1.total_revenue).to eq()
+      visit admin_invoice_path(invoice_2)
+
+      within "#total-revenue" do
+        expect(page).to have_content(invoice_2.total_revenue)
+      end
     end
 
     it 'And I see the total discounted revenue from this invoice which includes bulk discounts in the calculation' do
+      merchant_1 = create(:merchant)
+      merchant_2 = create(:merchant)
 
+      item_1 = create(:item, merchant: merchant_1)
+      item_2 = create(:item, merchant: merchant_1)
+      item_3 = create(:item, merchant: merchant_1)
+
+      item_4 = create(:item, merchant: merchant_2)
+      item_5 = create(:item, merchant: merchant_2)
+      item_6 = create(:item, merchant: merchant_2)
+
+      invoice_1 = create(:invoice)
+      invoice_2 = create(:invoice)
+
+      create(:invoice_items, invoice_id: invoice_1.id, item_id: item_1.id, unit_price: 100, quantity: 10) # 1000
+      create(:invoice_items, invoice_id: invoice_1.id, item_id: item_2.id, unit_price: 200, quantity: 20) # 4000
+      create(:invoice_items, invoice_id: invoice_1.id, item_id: item_3.id, unit_price: 150, quantity: 5) # 750
+
+      create(:invoice_items, invoice_id: invoice_2.id, item_id: item_4.id, unit_price: 50, quantity: 5) # 250
+      create(:invoice_items, invoice_id: invoice_2.id, item_id: item_5.id, unit_price: 75, quantity: 15) # 1125
+      create(:invoice_items, invoice_id: invoice_2.id, item_id: item_6.id, unit_price: 15, quantity: 10) # 150
+
+      bulk_discount_1 = create(:bulk_discount, merchant: merchant_1, discount: 0.25, threshold: 10)
+      bulk_discount_2 = create(:bulk_discount, merchant: merchant_2, discount: 0.10, threshold: 5)
+
+      visit admin_invoice_path(invoice_1)
+      
+      within "#total-discounted-revenue" do
+        expect(page).to have_content(invoice_1.total_discounted_revenue)
+      end
     end
   end
 end
--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -197,7 +197,7 @@ RSpec.describe Invoice, type: :model do
     end
 
     describe 'total_revenue' do
-      it 'has total revenue of invoice regardless of transaction status' do
+      it 'returns total revenue of invoice regardless of transaction status' do
         merchant_1 = create(:merchant)
         merchant_2 = create(:merchant)
   
@@ -222,6 +222,38 @@ RSpec.describe Invoice, type: :model do
 
         expect(invoice_1.total_revenue).to eq(5750)
         expect(invoice_2.total_revenue).to eq(1525)
+      end
+    end
+
+    describe 'total_discounted_revenue' do
+      it 'returns total discounted revenue of invoice' do
+        merchant_1 = create(:merchant)
+        merchant_2 = create(:merchant)
+  
+        item_1 = create(:item, merchant: merchant_1)
+        item_2 = create(:item, merchant: merchant_1)
+        item_3 = create(:item, merchant: merchant_1)
+  
+        item_4 = create(:item, merchant: merchant_2)
+        item_5 = create(:item, merchant: merchant_2)
+        item_6 = create(:item, merchant: merchant_2)
+  
+        invoice_1 = create(:invoice)
+        invoice_2 = create(:invoice)
+  
+        create(:invoice_items, invoice_id: invoice_1.id, item_id: item_1.id, unit_price: 100, quantity: 10) # 1000
+        create(:invoice_items, invoice_id: invoice_1.id, item_id: item_2.id, unit_price: 200, quantity: 20) # 4000
+        create(:invoice_items, invoice_id: invoice_1.id, item_id: item_3.id, unit_price: 150, quantity: 5) # 750
+  
+        create(:invoice_items, invoice_id: invoice_2.id, item_id: item_4.id, unit_price: 50, quantity: 5) # 250
+        create(:invoice_items, invoice_id: invoice_2.id, item_id: item_5.id, unit_price: 75, quantity: 15) # 1125
+        create(:invoice_items, invoice_id: invoice_2.id, item_id: item_6.id, unit_price: 15, quantity: 10) # 150
+
+        bulk_discount_1 = create(:bulk_discount, merchant: merchant_1, discount: 0.25, threshold: 10)
+        bulk_discount_2 = create(:bulk_discount, merchant: merchant_2, discount: 0.10, threshold: 5)
+
+        expect(invoice_1.total_discounted_revenue).to eq(4500)
+        expect(invoice_2.total_discounted_revenue).to eq(1372.5)
       end
     end
   end
